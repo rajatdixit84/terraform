d26a1caef6549b93f3af0e74a4b18c67efea5302
Rajat Dixit <rajatdixit@Rajats-MacBook-Pro.local>
2021-02-07 22:42:17 +0800

    chng resource

--- resources.tf
+++ resources.tf
@@ -1,10 +1,5 @@
 resource "aws_vpc" "environment-terraform" {
-
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
-    tags 
-        {  
-            Name = "terraform-aws-vpc"
-        }
 }
